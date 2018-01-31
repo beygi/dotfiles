86f4ec6894dbd3a9ce88d11ae6cdaef7e8394f5d
Mahdy Beygi <mahdy@beygi.org>
2018-01-31 18:50:55 +0330

    update runfiles

--- run-desktop.sh
+++ run-desktop.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
 sudo add-apt-repository ppa:papirus/papirus
 sudo apt-get update
-sudo apt install guake amarok cinnamon papirus-icon-theme fonts-powerline
-
-#install powerline fonts
-#git clone https://github.com/powerline/fonts.git --depth=1
-#cd fonts
-#./install.sh
-#cd ..
-#rm -rf fonts
-
-cd ~/.oh-my-zsh/custom/themes/powerlevel9k
-git pull
+sudo apt install guake amarok cinnamon papirus-icon-theme
--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo apt install python-pip autojump zsh libfribidi-bin jq
+sudo apt install python-pip autojump zsh libfribidi-bin jq curl
 sudo pip install jdate virtualenvwrapper
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
-chsh -s /usr/bin/zsh
+git checkout .zshrc
