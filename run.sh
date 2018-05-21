d7c99c1aa4057988568821d3e5135c57532a2738
Mahdy Beygi <mahdy@beygi.org>
2018-05-21 16:01:10 +0430

    update desktop and server installer

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt install python-pip autojump zsh libfribidi-bin jq curl
+sudo apt install python-pip autojump zsh libfribidi-bin jq curl thefuck
 sudo pip install jdate virtualenvwrapper
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
