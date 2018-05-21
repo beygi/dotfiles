deefdc773044b0bd29b7d946c3212790b26be5d6
Mahdy Beygi <mahdy@beygi.org>
2018-05-21 16:08:15 +0430

    add gitg

--- run-desktop.sh
+++ run-desktop.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo add-apt-repository ppa:papirus/papirus
 sudo apt-get update
-sudo apt install guake amarok cinnamon papirus-icon-theme smplayer
+sudo apt install guake amarok cinnamon papirus-icon-theme smplayer gitg
