2284993ba5c1894adc5233854f8c3652426bc983
Mahdy Beygi <mahdy@beygi.org>
2019-04-30 11:34:57 +0430

    change default shell to zsh

--- run.sh
+++ run.sh
@@ -4,3 +4,4 @@ sudo pip install jdate virtualenvwrapper
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 git checkout .zshrc
+chsh -s $(which zsh)
